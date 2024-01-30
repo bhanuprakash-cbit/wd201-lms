a5aef1ee984219899e842ab5414f54b6e8e5c4af
Bhanu Prakash <bhanuprakash.mymail@gmail.com>
2024-01-30 21:17:59 +0530

    Updated views

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.set("views", path.join(__dirname, "views"))
 
 app.get("/", (req, res) => {
     res.render("index", {
-        title: "LMS"
+        title: "Home Page"
     });
 });
 
@@ -24,6 +24,35 @@ app.get("/signin", (req,res) => {
     res.render("signin", {
         title: "SingIn"
     })
+})
+
+app.get("/signout", (req,res,next) => {
+    req.logout((err) => {
+      if (err) { return next(err) }
+      res.redirect("/")
+    })
+})
+
+app.get("/home", (req,res) => {
+    res.render("home")
+})
+
+app.get("/course", (req,res) => {
+res.render("course", {
+    chapters: "Chapter 1"
+})
+})
+
+app.get("/reports", (req,res) => {
+res.render("reports")
+})
+
+app.get("/newCourse", (req,res) => {
+res.render("newCourse")
+})
+
+app.get("/newCourse", (req,res) => {
+    res.render("newCourse")
 })
 
 module.exports = app
