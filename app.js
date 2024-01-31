27cea811d9eb4e3a0c1e75dac38c3877a56558f8
Bhanu Prakash <bhanuprakash.mymail@gmail.com>
2024-01-31 19:28:18 +0530

    Updated app.js

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
+const bodyParser = require("body-parser");
 const path = require("path")
+
+app.use(bodyParser.json());
 
 app.set("view engine", "ejs");
 
